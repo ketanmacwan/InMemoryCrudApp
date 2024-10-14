3b6ee802fc5aa56a1c0f3005e2c3e68a47142029
joy <joymacwan22@gmail.com>
2024-10-14 12:39:03 +0530

    Add project files

---
+++ InMemoryCrudApp/Controllers/BooksController.cs
@@ -0,0 +1,96 @@
+﻿using InMemoryCrudApp.Models;
+using Microsoft.AspNetCore.Mvc;
+using System.Collections.Generic;
+
+
+namespace InMemoryCrudApp.Controllers
+{
+    public class BooksController : Controller
+    {
+        private readonly BookRepository _repository;
+
+        public BooksController()
+        {
+            _repository = new BookRepository();
+        }
+
+        public IActionResult Index()
+        {
+            List<Book> books = _repository.GetAll();
+            return View(books);
+        }
+
+        public IActionResult Create()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public IActionResult Create([Bind("Id,Title,Author,Year")] Book book)
+        {
+            if (ModelState.IsValid)
+            {
+                _repository.Add(book);
+                return RedirectToAction(nameof(Index));
+            }
+            return View(book);
+        }
+
+        public IActionResult Edit(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var book = _repository.GetById(id.Value);
+            if (book == null)
+            {
+                return NotFound();
+            }
+            return View(book);
+        }
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public IActionResult Edit(int id, [Bind("Id,Title,Author,Year")] Book book)
+        {
+            if (id != book.Id)
+            {
+                return NotFound();
+            }
+
+            if (ModelState.IsValid)
+            {
+                _repository.Update(book);
+                return RedirectToAction(nameof(Index));
+            }
+            return View(book);
+        }
+
+        public IActionResult Delete(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var book = _repository.GetById(id.Value);
+            if (book == null)
+            {
+                return NotFound();
+            }
+
+            return View(book);
+        }
+
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public IActionResult DeleteConfirmed(int id)
+        {
+            _repository.Delete(id);
+            return RedirectToAction(nameof(Index));
+        }
+    }
+}
---
+++ InMemoryCrudApp/Models/BookRepository.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+
+namespace InMemoryCrudApp.Models
+{
+    public class BookRepository
+    {
+        private static List<Book> Books = new List<Book>
+        {
+            new Book { Id = 1, Title = "Book 1", Author = "Author 1", Year = 2001 },
+            new Book { Id = 2, Title = "Book 2", Author = "Author 2", Year = 2002 }
+        };
+            
+        public List<Book> GetAll() => Books;
+
+        public Book GetById(int id) => Books.FirstOrDefault(b => b.Id == id);
+
+        public void Add(Book book)
+        { 
+            book.Id = Books.Max(b => b.Id) + 1;
+            Books.Add(book); 
+        }
+
+        public void Update(Book book) { 
+            var existingBook = Books.FirstOrDefault(b => b.Id == book.Id);
+            if (existingBook != null)
+            {
+                existingBook.Title = book.Title;
+                existingBook.Author = book.Author;
+                existingBook.Year = book.Year;
+            }
+        }
+
+        public void Delete(int id) { 
+            var book = Books.FirstOrDefault(a => a.Id == id);
+            if (book != null) { 
+                Books.Remove(book);
+            }
+        }
+    }
+}
